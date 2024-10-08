76b8c0e9ee3a34c723880084598e1c53847f8c9c
NCPE227 <perrynoahchristian@gmail.com>
2024-10-08 15:40:22 -0400

    remaining updates fo today.

--- initial-experiment.py
+++ initial-experiment.py
@@ -5,6 +5,11 @@ from langchain_caai.caai_emb_client import caai_emb_client
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings
+
 load_dotenv()
 
 api_key = os.getenv('LLMF-API-KEY')
@@ -35,12 +40,43 @@ def process_query(query):
     return response
 
 
-def main():
+# Query for Vector DB
+def process_vector_query(query):
+    raw_documents = TextLoader('arxiv-metadata-oai-snapshot.json').load()
+    text_splitter = CharacterTextSplitter(chunk_size=200, chunk_overlap=10)
+    documents = text_splitter.split_documents(raw_documents)
+    db = FAISS.from_documents(documents, embeddings)
+
+    # Similarity search
+    docs = db.similarity_search(query)
+    ss = docs[0].page_content
+
+    # Similarity search by vector
+    embedding_vector = embeddings.embed_query(query)
+    docs = db.similarity_search_by_vector(embedding_vector)
+    ssv = docs[0].page_content
+
+    return ss, ssv
+
+
+def standard_query_test():
     test_query = "Explain to me what cancer is."
     print(f'Question: {test_query}')
     print(f'Response: {process_query(test_query)}')
     return
 
 
+def vector_db_query_test():
+    try:
+        query = "Can you find anything related to cancer?"
+        ss, ssv = process_vector_query(query)
+        print('text:', query)
+        print('Similarity search:[', ss, ']')
+        print('Similarity search by vector:[', ssv, ']')
+    except Exception as e:
+        print(e)
+    return
+
+
 if __name__ == "__main__":
-    main()
+    vector_db_query_test()
--- readme.md
+++ readme.md
@@ -3,4 +3,15 @@ If you intend to build the following project for your own uses, know that the da
 at the following locations:
 
 1. ARXIV Corpus Metadata: https://www.kaggle.com/datasets/Cornell-University/arxiv?resource=download
-2. 
+2. PubMed Diabetes Dataset: https://linqs.org/datasets/#pubmed-diabetes
+
+Citations and Considerations
+
+@conference{namata:mlg12,
+    title = {Query-Driven Active Surveying for Collective Classification},
+    author = {Galileo Mark Namata, Ben London, Lise Getoor, and Bert Huang},
+    booktitle = {International Workshop on Mining and Learning with Graphs},
+    year = {2012},
+    _publisher = {MLG},
+    address = {Edinburgh, Scotland},
+}

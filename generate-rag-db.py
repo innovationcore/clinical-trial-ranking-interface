c7bcfd636959fda625d68838bd085d075d9e0f73
NCPE227 <perrynoahchristian@gmail.com>
2024-10-08 13:59:16 -0400

    initial upload

---
+++ generate-rag-db.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+def LLM_Factory_Connection():
+    api_key = os.getenv('LLMF-API-KEY')  # Load API key
+    return
+
+
+def main():
+    print('Code Enabled. B-)')
+    return
+
+
+if __name__ == "__main__":
+    main()
